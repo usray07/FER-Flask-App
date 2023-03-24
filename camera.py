440d7153125a1a732827aa766406c8d03808cee9
usray07 <usray0706@gmail.com>
2023-03-24 22:24:58 +0530

    Updating for multiple facial emotions

--- camera.py
+++ camera.py
@@ -25,14 +25,11 @@ class VideoCamera(object):
         sub_img = frame[0:int(height/6),0:int(width)]
         
         black_rect = np.ones(sub_img.shape, dtype=np.uint8)*0
-        res = cv2.addWeighted(sub_img, 0.77, black_rect,0.23, 0)
+        res = cv2.addWeighted(sub_img, 0.7, black_rect,1, 0)
         FONT = cv2.FONT_HERSHEY_SIMPLEX
 
         FONT_SCALE = 0.8
         FONT_THICKNESS = 2
-
-        lable_color = (10, 10, 255)
-
         try:
 
             for (x,y, w, h) in faces:
@@ -43,20 +40,19 @@ class VideoCamera(object):
                 image_pixels = np.expand_dims(image_pixels, axis = 0)
                 image_pixels /= 255
                 pred = model.predict_emotion(image_pixels)
-            lable = "Face Detected"
+                cv2.putText(frame, "{}".format(pred), (x+w+2,y+h+2), FONT,0.7, (240,16,255),2)  
+
+            lable = "Face(s) Detected"
             lable_dimension = cv2.getTextSize(lable,FONT ,FONT_SCALE,FONT_THICKNESS)[0]
             textX = int((res.shape[1] - lable_dimension[0]) / 2)
             textY = int((res.shape[0] + lable_dimension[1]) / 2)
-            cv2.putText(res, lable, (textX,textY), FONT, FONT_SCALE, (0,0,0), FONT_THICKNESS)
-            cv2.putText(res, "Sentiment: {}".format(pred), (0,textY+22+5), FONT,0.7, lable_color,2)  
-
+            cv2.putText(res, lable, (textX,textY), FONT, FONT_SCALE, (238,244,21), FONT_THICKNESS)
+            cv2.putText(res, " ".format(pred), (0,textY+22+5), FONT,0.7, (238,244,21),2)  
+            
   
         except:
             black_rect = np.ones(sub_img.shape, dtype=np.uint8)*0
-            res = cv2.addWeighted(sub_img, 0.77, black_rect,0.23, 0)
-        
-            cv2.putText(res, " ", (textX,textY), FONT, FONT_SCALE, (0,0,250), FONT_THICKNESS)
-
+            res = cv2.addWeighted(sub_img, 0.7, black_rect,1, 0)
             lable = "No Face Detected!! "
             lable_dimension = cv2.getTextSize(lable,FONT ,FONT_SCALE,FONT_THICKNESS)[0]
             textX = int((res.shape[1] - lable_dimension[0]) / 2)
